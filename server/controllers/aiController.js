2f6653177ba2f4b0abc086004dac5c9d292a7e50
Akash-Behera-cmd <akashbehera955649@gmail.com>
2026-03-20 10:23:08 +0530

    update backend

---
+++ server/controllers/aiController.js
@@ -0,0 +1,165 @@
+//Controller for enhancing a resumes professional summary
+//POST: /api/ai/enhance-pro-sum
+
+import ai from "../configs/ai.js";
+import Resume from "../models/Resume.js";
+
+export const enhanceProfessionalSummary = async(req,res)=>{
+     try {
+        const  {userContent}= req.body;
+        if(!userContent){
+            return res.status(400).json({message:'Missing required fields'})
+        }
+
+        const response=await ai.chat.completions.create({
+            model: process.env.OPENAI_MODEL,
+            messages: [
+        {   role: "system",
+            content: "You  are an expert in resume  writing.Your  task is to enhance the professional summary of a resume. The summary should be 1-2 sentences  also highlighting key skills,experience,and career objectives. Make it compelling and ATS-frindly. and only return text no options or anything else." 
+        },
+        {
+            role: "user",
+            content: userContent,
+        },
+          ],
+        })
+
+        const enhancedContent =
+        response?.choices?.[0]?.message?.content || "No response generated";
+
+        return res.status(200).json({enhancedContent})
+
+     } catch (error) {
+        return res.status(500).json({message:error.message})
+     }
+}
+
+
+//controller  for enhancing a resume's job description
+//POST: /api/ai/enhance-job-desc
+
+export const enhanceJobDescription = async(req,res)=>{
+     try {
+        const  {userContent}= req.body;
+        if(!userContent){
+            return res.status(400).json({message:'Missing required fields'})
+        }
+
+        const response=await ai.chat.completions.create({
+            model: process.env.OPENAI_MODEL,
+            messages: [
+        {   role: "system",
+            content: "You  are an expert in resume  writing.Your  task is to enhance the job description of a resume. The job description should be only in 1-2 sentence also highlighting key responsibilities and achievents.Use action verbs and  quantifiable results where possible .Make it ATS-friendly .and only return text no options or anything else." 
+ },
+        {
+            role: "user",
+            content: userContent,
+        },
+          ],
+        })
+
+        const enhancedContent =
+        response?.choices?.[0]?.message?.content || "No response generated";
+
+        return res.status(200).json({enhancedContent})
+
+     } catch (error) {
+        return res.status(500).json({message:error.message})
+     }
+}
+
+
+//controller for  uploading a resume to the database
+//POST: /api/ai/upload-resume
+
+export const uploadResume = async(req,res)=>{
+     try {
+        const {resumeText,title}=  req.body;
+        const userId= req.userId;
+
+        if(!resumeText){
+            return  res.status(400).json({message:'Missing required fields'})
+        }
+        
+        const systemPrompt = "You are an expert AI Agent  to extract data from resume."
+
+        
+        const userPrompt =`extract data from this resume: ${resumeText}
+
+provide data in the following JSON format with no additional text before or after:
+
+{
+  "professionalSummary": "",
+  "skills": [],
+  "personalInfo": {
+    "image": "",
+    "fullName": "",
+    "profession": "",
+    "email": "",
+    "phone": "",
+    "location": "",
+    "linkedin": "",
+    "website": ""
+  },
+  "experience": [
+    {
+      "company": "",
+      "position": "",
+      "startDate": "",
+      "endDate": "",
+      "description": "",
+      "isCurrent": false
+    }
+  ],
+  "projects": [
+    {
+      "name": "",
+      "type": "",
+      "description": ""
+    }
+  ],
+  "education": [
+    {
+      "institution": "",
+      "degree": "",
+      "field": "",
+      "graduationDate": "",
+      "gpa": ""
+    }
+  ]
+}`;
+
+        const response=await ai.chat.completions.create({
+            model: process.env.OPENAI_MODEL,
+            messages: [
+        {   role: "system",
+            content: systemPrompt 
+ },
+        {
+            role: "user",
+            content: userPrompt,
+        },
+          ],
+          response_format: {type: 'json_object'}
+        })
+
+        
+        const extractedData =
+        response?.choices?.[0]?.message?.content?.trim() || "{}";
+
+        
+        let parseData = {};
+        try {
+            parseData = JSON.parse(extractedData);
+        } catch (err) {
+            return res.status(500).json({ message: "Invalid JSON from AI" });
+        }
+
+        const newResume =await Resume.create({userId,title,...parseData})
+
+        res.json({resumeId: newResume._id})
+
+     } catch (error) {
+        return res.status(500).json({message:error.message})
+     }
+}
