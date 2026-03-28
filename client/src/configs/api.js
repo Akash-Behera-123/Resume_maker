09a5b895cd70372ce3063d7c1652a8fb280bde8f
Akash-Behera-cmd <akashbehera955649@gmail.com>
2026-03-29 01:17:28 +0530

    api.js credentials false

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL, // always read from environment variable
-  withCredentials: true,                  // allows cookies if needed
+  withCredentials: false,                  // allows cookies if needed
   headers: {
     'Content-Type': 'application/json'
   }
